64be0d7f011894d369699be7c70dff7be6d6d6b3
huespixelstorm <parantargretchgeen@gmail.com>
2026-05-12 19:32:45 +0800

    baselogic done, need to add multiple rounds

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
+let choices = ["rock", "paper", "scissors"];
+
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"];
-    return choices[Math.floor(Math.random() * 3)];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+//  Player Prompts
+alert("Rock Paper Scissors! press 'ok' to play");
+let playerName = prompt("Enter your Player name: ");
+
+function getPlayerChoice() {
+    let choice = prompt("Pick either, 'Rock', 'Paper', 'Scissors'").toLowerCase()
+
+        if (choice != "rock" && choice != "paper" && choice != "scissors")
+            return choice = prompt("choice is INVALID, choose from the options given, 'Rock', 'Paper', or 'Scissors'")
+        else return choice
 }
 
-function results(player, computer) {
-    if (player === computer)
-        return "It's a Draw";
+//  Win-Loss-Draw Conditions
+function getRoundResult() {
 
-    if (
-    (player === "rock" && computer === "scissors") ||
-    (player === "scissors" && computer === "paper") ||
-    (player === "paper" && computer === "rock") 
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+
+    // Draw
+    if (playerChoice === "rock" && computerChoice === "rock" ||
+        playerChoice === "paper" && computerChoice === "paper" ||
+        playerChoice === "scissors" && computerChoice === "scissors"
     )
-    {
-        return "You Win!"
-    } else {
-        return "You Lose!"
-    }
+    return alert(`${playerName} and the comoputer chose the same move, It's a Draw.`)
+
+
+    // Win-Lose
+    if (playerChoice === "rock" && computerChoice === "scissors" ||
+        playerChoice === "paper" && computerChoice === "rock" ||
+        playerChoice === "scissors" && computerChoice === "paper"
+    )
+    return alert(
+        `${playerName} chose ${playerChoice}
+        Computer chose ${computerChoice}, You Won the round!`)
+    else alert(`${playerName} chose ${playerChoice}
+        Computer chose ${computerChoice}, You Lost the round...`)
 }
+console.log(getRoundResult())
