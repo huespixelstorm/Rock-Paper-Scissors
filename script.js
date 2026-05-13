bb9c26c3a8b7124ff5e62d4ccb5f7199edb24541
huespixelstorm <parantargretchgeen@gmail.com>
2026-05-13 23:01:16 +0800

    logic done
    might add a GUI when ik how

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let choices = ["rock", "paper", "scissors"];
+let playerRoundsWon = 0;
+let computerRoundsWon = 0;
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
@@ -10,10 +12,14 @@ let playerName = prompt("Enter your Player name: ");
 function getPlayerChoice() {
     let choice = prompt("Pick either, 'Rock', 'Paper', 'Scissors'").toLowerCase()
 
-        if (choice != "rock" && choice != "paper" && choice != "scissors")
-            return choice = prompt("choice is INVALID, choose from the options given, 'Rock', 'Paper', or 'Scissors'")
-        else return choice
+        while (
+        choice !== "rock" &&
+        choice !== "paper" &&
+        choice !== "scissors") {
+            choice = prompt('choice is INVALID, please pick either, "Rock", "Paper", "Scissors".').toLowerCase()
+        } return choice
 }
+
 
 //  Win-Loss-Draw Conditions
 function getRoundResult() {
@@ -21,23 +27,45 @@ function getRoundResult() {
     let playerChoice = getPlayerChoice();
     let computerChoice = getComputerChoice();
 
-    // Draw
-    if (playerChoice === "rock" && computerChoice === "rock" ||
-        playerChoice === "paper" && computerChoice === "paper" ||
-        playerChoice === "scissors" && computerChoice === "scissors"
-    )
-    return alert(`${playerName} and the comoputer chose the same move, It's a Draw.`)
-
+    if (playerChoice === computerChoice)
+    return alert(`"${playerName}" and the computer chose the same move, It's a Draw.`)
 
     // Win-Lose
     if (playerChoice === "rock" && computerChoice === "scissors" ||
         playerChoice === "paper" && computerChoice === "rock" ||
-        playerChoice === "scissors" && computerChoice === "paper"
-    )
-    return alert(
-        `${playerName} chose ${playerChoice}
-        Computer chose ${computerChoice}, You Won the round!`)
-    else alert(`${playerName} chose ${playerChoice}
-        Computer chose ${computerChoice}, You Lost the round...`)
+        playerChoice === "scissors" && computerChoice === "paper") {
+    
+        playerRoundsWon++
+            
+        return alert(`${playerName} chose ${playerChoice}
+        Computer chose ${computerChoice},
+         You Won the round!
+         score: ${playerRoundsWon} - ${computerRoundsWon}`)
+
+        } else {
+        
+        computerRoundsWon++
+            
+        alert(`${playerName} chose ${playerChoice}
+        Computer chose ${computerChoice},
+         You Lost the round...
+         score: ${playerRoundsWon} - ${computerRoundsWon}`) }
 }
-console.log(getRoundResult())
+
+while (playerRoundsWon < 5 && computerRoundsWon < 5) {
+    getRoundResult();
+}
+
+matchWinner();  
+
+//  Match Results/determiner
+function matchWinner() {
+    if (playerRoundsWon === 5) {
+        return alert("You Won the game!")
+    } else {
+        if (computerRoundsWon === 5) {
+            return alert("You Lost the game...")
+        }
+
+    }
+}
